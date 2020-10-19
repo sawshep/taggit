8e470096acd35aa053f12deef631461062f78752
Sawyer Shepherd <contact@sawyershepherd.org>
2020-10-19 15:12:13 -0400

    design

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
-use clap::{load_yaml, App};
+/**
+ * A command-line file tagging/archiving utility.
+ */
+/* Some `unwrap()` statements do not need
+ * to be handled due to clap. In all cases,
+ * an argument is either required or has
+ * a default value.
+ */
+use clap::{load_yaml, App, ArgMatches};
 use serde::{Deserialize, Serialize};
+use serde_json;
 use sha1::Sha1;
-use std::fs::{create_dir_all, set_permissions, File};
+use std::ffi::OsStr;
+use std::fs;
 use std::io::{Error, Read};
 use std::path::{Path, PathBuf};
 use std::process::exit;
 
-#[derive(Serialize, Deserialize)]
-struct Entry {
-    id: u32,
-    name: String,
+//#[derive(Serialize, Deserialize, Debug)]
+struct Entry<'a> {
     hash: String,
-    tags: Vec<String>,
+    names: &'a Vec<&'a OsStr>,
+    tags: &'a Vec<&'a str>,
 }
+
+const TAGGIT_FOLDER: &'static str = ".taggit";
 
 /**
  * The exit function has been separated from `taggit()`
@@ -49,49 +60,79 @@ fn taggit() -> i32 {
 /**
  * Initialize a new Taggit archive, either in an existing folder or a new one.
  */
-fn init(matches: &clap::ArgMatches) -> i32 {
-    if matches.is_present("directory") {
-        let directory: &str = matches.value_of("directory").unwrap();
-        let target = Path::new(directory);
-        let taggit: PathBuf = target.join(".taggit");
-        if taggit.exists() {
-            eprintln!("Error: Taggit archive already exists in {}", directory);
+fn init(matches: &ArgMatches) -> i32 {
+    let input_dir: &str = matches.value_of("directory").unwrap();
+    let target_path = Path::new(&input_dir);
+    let taggit_path: PathBuf = target_path.join(TAGGIT_FOLDER);
+    if taggit_path.exists() {
+        eprintln!("Error: Taggit archive already exists in {}", &input_dir);
+        return 1;
+    }
+    match fs::create_dir_all(taggit_path.join("files")) {
+        Ok(s) => s,
+        Err(_) => {
+            eprintln!("Error: could not create Taggit archive in {}", input_dir);
             return 1;
         }
-        match create_dir_all(&taggit) {
-            Ok(s) => s,
-            Err(_) => {
-                eprintln!("Error: could not create Taggit archive in {}", directory);
-                return 1;
-            }
-        }
-        println!("Initialized Taggit archive in {}", taggit.to_string_lossy());
     }
+
+    println!("Initialized Taggit archive in {}", input_dir);
     0
 }
 
 /**
  * Add files to be tracked by Taggit, adding optional tags.
  */
-fn add(matches: &clap::ArgMatches) -> i32 {
-    let files: Vec<_> = matches.values_of("files").unwrap().collect();
-    for file in files {
-        let path = PathBuf::from(file);
-        if path.is_file() {
-            match checksum(path) {
-                Ok(hash) => println!("{}", hash),
-                Err(_) => {
-                    eprintln!("Error: could not calculate hash of {}", file);
-                    continue;
-                }
-            };
-        } else {
-            eprintln!("Cannot add {} to archive: is a directory", file);
-        }
+fn add(matches: &ArgMatches) -> i32 {
+    // Never panics
+    let input_dir: &str = matches.value_of("archive").unwrap();
+    let taggit_path = Path::new(input_dir).join(TAGGIT_FOLDER);
+    if !taggit_path.is_dir() {
+        eprintln!("Taggit archive does not exist in {}", input_dir);
+        return 1;
     }
-    if matches.is_present("tags") {
-        let tags: Vec<_> = matches.values_of("tags").unwrap().collect();
-        println!("{:?}", tags);
+    // Never panics
+    let input_files: Vec<&str> = matches.values_of("files").unwrap().collect();
+    let structs_path = taggit_path.join("structs");
+    let mut structs = match fs::OpenOptions::new()
+        .read(true)
+        .append(true)
+        .create(true)
+        .open(&structs_path)
+    {
+        Ok(s) => s,
+        Err(_) => {
+            eprintln!(
+                "Error: could not open structures file at {}",
+                structs_path.to_string_lossy()
+            );
+            return 1;
+        }
+    };
+    for input_file in input_files {
+        let file_path = Path::new(input_file);
+        if !file_path.is_file() {
+            eprintln!("Cannot add {} to archive: is a directory", input_file);
+            continue;
+        }
+        let hash = match checksum(&file_path) {
+            Ok(h) => h,
+            Err(_) => {
+                eprintln!("Cannot open {}, skipping", input_file);
+                continue;
+            }
+        };
+        let name = &file_path.file_name().unwrap();
+        let mut entry = Entry {
+            hash: hash,
+            names: &vec![&name],
+            tags: &vec![],
+        };
+        if matches.is_present("tags") {
+            let input_tags: Vec<&str> = matches.values_of("tags").unwrap().collect();
+            println!("{:?}", &input_tags);
+            entry.tags = &input_tags;
+        }
     }
     0
 }
@@ -99,8 +140,8 @@ fn add(matches: &clap::ArgMatches) -> i32 {
 /**
  * Find the checksum of a file.
  */
-fn checksum(path: PathBuf) -> Result<String, Error> {
-    let mut file = match File::open(path) {
+fn checksum(path: &Path) -> Result<String, Error> {
+    let mut file = match fs::File::open(path) {
         Ok(f) => f,
         Err(e) => return Err(e),
     };
@@ -114,17 +155,27 @@ fn checksum(path: PathBuf) -> Result<String, Error> {
     Ok(hasher.digest().to_string())
 }
 
+//fn serialize(path: PathBuf) {}
+
 /**
  * Manage tags associated with checksums.
  */
-fn tag(matches: &clap::ArgMatches) -> i32 {
-    let checksums: Vec<_> = matches.values_of("checksums").unwrap().collect();
+fn tag(matches: &ArgMatches) -> i32 {
+    let input_dir: &str = matches.value_of("archive").unwrap();
+    let taggit_path = Path::new(input_dir).join(TAGGIT_FOLDER);
+    if !taggit_path.is_dir() {
+        eprintln!("Taggit archive does not exist in {}", input_dir);
+        return 1;
+    }
+    let checksums: Vec<&str> = matches.values_of("checksums").unwrap().collect();
+    // ^ TODO: Make user interface with tag work based on file IDs, not checksums.
+    // (Still make tags associate with checksums, though)
     if matches.is_present("add") {
-        let tags: Vec<_> = matches.values_of("add").unwrap().collect();
+        let tags: Vec<&str> = matches.values_of("add").unwrap().collect();
         println!("Associated {:?} with {:?}", tags, checksums);
     }
     if matches.is_present("delete") {
-        let tags: Vec<_> = matches.values_of("delete").unwrap().collect();
+        let tags: Vec<&str> = matches.values_of("delete").unwrap().collect();
         println!("Dissociated {:?} with {:?}", tags, checksums);
     }
     0
@@ -133,13 +184,20 @@ fn tag(matches: &clap::ArgMatches) -> i32 {
 /**
  * Query the list of checksums tracked by Taggit, optionally filtering by name and tags.
  */
-fn list(matches: &clap::ArgMatches) -> i32 {
+fn list(matches: &ArgMatches) -> i32 {
+    let input_dir: &str = matches.value_of("archive").unwrap();
+    let taggit_path = Path::new(input_dir).join(TAGGIT_FOLDER);
+    if !taggit_path.is_dir() {
+        eprintln!("Taggit archive does not exist in {}", input_dir);
+        return 1;
+    }
     if matches.is_present("name") {
         let name: &str = matches.value_of("name").unwrap();
-        println!("Sorting by {:?}", name);
+        println!("Sorting by {}", name);
     }
     if matches.is_present("tags") {
-        let _tags: Vec<_> = matches.values_of("tags").unwrap().collect();
+        let tags: Vec<&str> = matches.values_of("tags").unwrap().collect();
+        println!("Sorting by {:?}", tags);
     }
     0
 }
